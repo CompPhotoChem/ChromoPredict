19e7bcf666018dd7e75a25bcebb5a75c7a46bc64
CPC Group <138974371+CompPhotoChem@users.noreply.github.com>
2025-06-13 20:36:30 +0200

    Update chromfactors.py

--- src/chromopredict/chromfactors.py
+++ src/chromopredict/chromfactors.py
@@ -5,6 +5,33 @@ factors = {
     "endocyclic_double_bond": "[#6;R]=[#6;R]"
 }
 
+# Woodward values
+woodward_factor_values = { #https://de.wikipedia.org/wiki/Woodward-Fieser-Regeln
+    "conjugated_double_bond": 30,
+    "exocyclic_double_bond": 5,
+    "homoanular_cyclodiene": 39
+}
+
+# Fieser-Kuhn
+fieser_kuhn_factors = {
+    "alkyl" : "[#6;X4]",
+    "endocyclic_double_bond": "[#6;R]=[#6;R]"
+}
+
+fieser_kuhn_factor_values = { #https://de.wikipedia.org/wiki/Woodward-Fieser-Regeln
+    "endocyclic_double_bond": 1,
+    "alkyl" : 1
+}
+
+
+fieser_factor_values = { # doi: 10.1021/jo01164a003
+    "endocyclic_double_bond" : 30,
+    "conjugated_double_bond": 30,
+    "exocyclic_double_bond": 5,
+    "homoanular_cyclodiene": 39
+}
+
+
 fieser_kuhn_factors = {
     "alkyl" : "[#6;X4]",
     "endocyclic_double_bond": "[#6;R]=[#6;R]"
