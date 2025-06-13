4a1e7507cb104a62416ee4d5d8cf695fd3a784fe
CPC Group <138974371+CompPhotoChem@users.noreply.github.com>
2025-06-13 20:46:09 +0200

    Create solvent.py

---
+++ src/chromopredict/solvent.py
@@ -0,0 +1,11 @@
+# solvent correction values
+solvent_values = { #doi: 10.1021/ja01849a066
+    "O": -8,
+    "CO": -5,
+    "CCO": -1,
+    "ClC(Cl)Cl": 0,
+    "O1CCOCC1": 5,
+    "CCOCC": 7,
+    "CCCCCC": 11,
+    "C1CCCCC1": 11
+}
