dfd0095808d1950f7252ccb0d55b455c16694a48
CPC Group <138974371+CompPhotoChem@users.noreply.github.com>
2025-06-13 20:31:22 +0200

    Create chromfactors.py

---
+++ src/chromopredict/chromfactors.py
@@ -0,0 +1,11 @@
+
+factors = {
+    "homoanular_cyclodiene": "[#6]1=[#6]-[#6]=[#6]-[#6]-[#6]1",
+    "conjugated_double_bond": "[#6]=[#6]",
+    "endocyclic_double_bond": "[#6;R]=[#6;R]"
+}
+
+fieser_kuhn_factors = {
+    "alkyl" : "[#6;X4]",
+    "endocyclic_double_bond": "[#6;R]=[#6;R]"
+}
