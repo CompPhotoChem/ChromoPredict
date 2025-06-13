441c3a4c38052f9ad1babdfde91077ddeb1257ea
CPC Group <138974371+CompPhotoChem@users.noreply.github.com>
2025-06-13 20:34:42 +0200

    Update chrombase.py

--- src/chromopredict/chrombase.py
+++ src/chromopredict/chrombase.py
@@ -1,4 +1,3 @@
-
 general_rules = {
     "woodward_extended": "[#6]=[#6]-[#6](=[#8])-[#6]",
     "woodward": "[#6]=[#6]-[#6]=[#8]",
@@ -6,6 +5,7 @@ general_rules = {
     "fieser": "[#6]=[#6]-[#6]=[#6]"
 }
 
+# Woodward
 woodward_base = {
     "alpha_beta_aldehyd": "[#6]=[#6]-[#6;H1](=[#8])",
     "alpha_beta_ketone": "[#6]=[#6]-[#6](=[#8])-[#6]",
@@ -15,12 +15,33 @@ woodward_base = {
     "cyclopentenone": "[#6]1:[#6]:[#6]:[#6]:[#6]:1(=[#8])",
 }
 
+woodward_base_values = { # doi: textbook
+    "alpha_beta_aldehyd": 210,
+    "alpha_beta_ketone": 215,
+    "alpha_beta_ester": 195,
+    "cyclopentenone": 202,
+    "cyclohexanone": 215, # doi: 10.1021/jo01164a003
+    "alpha_beta_acid" : 195, # doi: 
+}
+
+# Fieser
 fieser_base = {
     "homoanular_cyclic": "[#6]1=[#6]-[#6]=[#6]-[#6]-[#6]1",
     "acyclic_diene": "[#6;!R]=[#6;!R]-[#6;!R]=[#6;!R]",
     "heteroanular_cyclic": "[#6]=[#6;R]-[#6;R]=[#6]"
 }
 
+fieser_base_values = { # doi: 10.1021/jo01164a003
+    "acyclic_diene": 217,
+    "homoanular_cyclic": 253,
+    "heteroanular_cyclic": 214
+}
+
+# Fieser Kuhn
 fieser_kuhn_base = {
     "polyene_4_conj": "[#6]=[#6]-[#6]=[#6]-[#6]=[#6]-[#6]=[#6]"
+}
+
+fieser_kuhn_base_values = { #https://de.wikipedia.org/wiki/Woodward-Fieser-Regeln
+    "polyene_4_conj": 114
 }
