ee8e956dcd689da0d4764e04e1f463e6e31a9134
CPC Group <138974371+CompPhotoChem@users.noreply.github.com>
2025-06-13 20:31:50 +0200

    Create chromsubst.py

---
+++ src/chromopredict/chromsubst.py
@@ -0,0 +1,19 @@
+
+fieser_subs = {
+    "dialkylamine": "[#7](-[#6])(-[#6])",
+    "thioether": "[#16]-[#6]",
+    "phenoxy": "[#8]-c1:[#6]:[#6]:[#6]:[#6]:[#6]:1",
+    "halogen": "[F,Cl,Br,I]",
+    "alkoxy": "[#8]-[#6]",
+    "alkyl": "[#6;X4]",
+    "carboxy": "[#6](=[#8])-[#8H]",
+}
+
+woodward_subs = {
+    "alkoxy": "[#8]-[#6]",
+    "alkyl": "[#6;X4]",
+    "bromo": "[Br]",
+    "carboxy": "[#6](=[#8])-[#8H]",
+    "chloro": "[Cl]",
+    "hydroxy": "[#8H]"
+}
